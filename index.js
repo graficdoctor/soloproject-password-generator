2ab3cbfe32a34c8e98e9371113fcaf055caecefb
Katrien S <ontwerp@katriens.be>
2023-04-25 10:30:38 +0200

    incl files

---
+++ index.js
@@ -0,0 +1,126 @@
+const characters = [
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+	'0',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'~',
+	'`',
+	'!',
+	'@',
+	'#',
+	'$',
+	'%',
+	'^',
+	'&',
+	'*',
+	'(',
+	')',
+	'_',
+	'-',
+	'+',
+	'=',
+	'{',
+	'[',
+	'}',
+	']',
+	',',
+	'|',
+	':',
+	';',
+	'<',
+	'>',
+	'.',
+	'?',
+	'/'
+];
+
+const fieldOne = document.getElementById('generated-password-1');
+const fieldTwo = document.getElementById('generated-password-2');
+const outputField = document.querySelectorAll('.outputfield');
+const generateBtnElem = document.getElementById('generate-btn');
+
+const generatePassword = (characters) => {
+	let generatedPasswords = [];
+	for (let i = 0; i < 2; i++) {
+		let generatedPassword = '';
+
+		while (generatedPassword.length < 15) {
+			const randomIndex = Math.floor(Math.random() * characters.length);
+			const randomCharacter = characters[randomIndex];
+			generatedPassword += randomCharacter;
+		}
+		generatedPasswords.push(generatedPassword);
+	}
+	fieldOne.textContent = generatedPasswords[0];
+	fieldTwo.textContent = generatedPasswords[1];
+	return generatedPasswords;
+};
+
+generateBtnElem.addEventListener('click', () => generatePassword(characters));
+
+function copyToClipboard(e) {
+	e.preventDefault();
+	navigator.clipboard.writeText(this.textContent);
+	// console.log(this.textContent);
+}
+
+fieldOne.addEventListener('click', copyToClipboard);
+fieldTwo.addEventListener('click', copyToClipboard);
